807efc77bd646e03a6b61e54ec739fb19abbf956
kevin010411 <kevin010411@gmail.com>
2024-12-05 19:39:27 +0800

    Try Azure SQL

--- custom_module/app.py
+++ custom_module/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +9,12 @@ UPLOAD_FOLDER = "./static/uploads"
 app.secret_key = 'secret_key'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111111k@127.0.0.1:5432/postgres"
+connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+if connection_string is None:
+    # 這是FU的本地postgres
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111111k@127.0.0.1:5432/postgres"
+else:
+    # 這是Azure的postgres
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
