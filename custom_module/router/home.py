058dcd874ae28ebd3eb9497acb6a2c22fb1dce68
kevin010411 <kevin010411@gmail.com>
2024-12-05 20:19:59 +0800

    Add Login Website

--- custom_module/router/home.py
+++ custom_module/router/home.py
@@ -1,6 +1,6 @@
 from custom_module import app
 from sqlalchemy import text
-from flask import render_template
+from flask import render_template, request, session
 
 from custom_module import User
 
@@ -17,3 +17,24 @@ def user_list():
     for data in user_data:
         print(type(data.name))
     return render_template("user_list.html", user_list=user_data)
+
+
+USER_CREDENTIALS = {
+    "username": "admin",
+    "password": "000000"
+}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
+            session['login'] = True
+            return render_template('home.html')
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
