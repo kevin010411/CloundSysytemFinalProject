7d03e6485795102f01095a8d9be505c6a281641b
kevin010411 <kevin010411@gmail.com>
2024-12-06 22:34:09 +0800

    try fix sql prob

--- custom_module/app.py
+++ custom_module/app.py
@@ -9,7 +9,7 @@ UPLOAD_FOLDER = "./static/uploads"
 app.secret_key = 'secret_key'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING_T')
 if connection_string is None:
     # 這是FU的本地postgres
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111111k@127.0.0.1:5432/postgres"
