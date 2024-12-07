b9b42429e2f93739c314bc10bef4355f6d6aeb8d
kevin010411 <kevin010411@gmail.com>
2024-12-07 08:18:29 +0800

    Fix /log bug

--- custom_module/app.py
+++ custom_module/app.py
@@ -19,17 +19,20 @@ except:
     log = f"不能訪問{connection_string_temp}"
 
 if connection_string is None:
+    connection_string = "dbname=media-database host=media-server.postgres.database.azure.com port=5432 sslmode=require user=htrwgnqpug password=yG4h138eOi1Q$LsP"
+
+if connection_string is None:
     # 這是FU的本地postgres
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111111k@127.0.0.1:5432/postgres"
 else:
     # 這是Azure的postgres
+    print(connection_string)
     app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-app.route("/log")
-
-
+@app.route("/log")
 def display_log():
     return render_template("log.html", log=log)
