55adfc65f98cf05f059ddf7754eea01e5a77ee0d
kevin010411 <kevin010411@gmail.com>
2024-12-07 08:31:03 +0800

    Parse Azure SQL link string

--- custom_module/app.py
+++ custom_module/app.py
@@ -10,22 +10,22 @@ UPLOAD_FOLDER = "./static/uploads"
 app.secret_key = 'secret_key'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING_T')
-connection_string_temp = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
-try:
-    connection = psycopg2.connect(connection_string_temp)
-    log = f"訪問成功{connection_string_temp}"
-except:
-    log = f"不能訪問{connection_string_temp}"
+connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
 
-if connection_string is None:
-    connection_string = "dbname=media-database host=media-server.postgres.database.azure.com port=5432 sslmode=require user=htrwgnqpug password=yG4h138eOi1Q$LsP"
+try:
+    connection = psycopg2.connect(connection_string)
+    log = f"訪問成功{connection_string}"
+except:
+    log = f"不能訪問{connection_string}"
 
 if connection_string is None:
     # 這是FU的本地postgres
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111111k@127.0.0.1:5432/postgres"
 else:
     # 這是Azure的postgres
+    param = dict(item.split("=")
+                 for item in connection_string.split(" "))
+    connection_string = f'postgresql://{param["user"]}:{param["password"]}@{param["host"]}:{param["port"]}/{param["dbname"]}?sslmode={param["sslmode"]}'
     print(connection_string)
     app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
 
